b5ea10a2f72dcb0991e35548b2dfc029d0388972
Md Arifuzzaman <md.arifuzzaman@citytech.cuny.edu>
2021-10-22 12:08:52 -0400

    ADD: Messenger Chat Feature + Services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <MessengerCustomerChat
+    pageId="524371708070058"
+    appId="2272918056294793"
+    
+  />
   );
 }
 
